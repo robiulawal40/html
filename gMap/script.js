7a3d44fde223c45273abddf4b01b5145c99a2c1b
robiulawal.40 <ral.sust@hotmail.com>
2018-09-28 06:24:56 +0600

    api added

--- gMap/script.js
+++ gMap/script.js
@@ -1,1 +1,22 @@
 //alert(1);
+/* gMap Api AIzaSyD5WrreVasrk_iFBApM505yVuf1PscioIw */
+function initMap() {
+ var options ={
+   center:{
+     lat: -25.344,
+     lng: 131.036
+   },
+   zoom: 4
+ },
+ element = document.getElementById('nz_map');
+
+// The map, centered
+var map = new google.maps.Map(element, options );
+
+var marker = new google.maps.Marker({
+          position: options.center,
+          map: map,
+          title: 'Hello World!'
+        });
+//var marker = new google.maps.Marker({position: uluru, map: map});
+}
