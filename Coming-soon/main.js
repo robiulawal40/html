a7b1fb3515ffd43c58b17cc39f1fd5169299f5ec
robiulawal.40 <ral.sust@hotmail.com>
2018-10-25 23:12:18 +0600

    Done

---
+++ Coming-soon/main.js
@@ -0,0 +1,11 @@
+const remaingTime = {
+  days : 10,
+  hours:3,
+  minutes: 30,
+  second:40
+}
+let days = document.querySelector('.days');
+window.addEventListener("load", function(){
+  console.log(days.innerHtml);
+
+});
